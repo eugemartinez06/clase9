c45d5ebcfa37103d7cc18c0a649bf79de0205c27
eugemartinez06 <eugemartinez06@gmail.com>
2024-05-16 18:05:54 -0300

    clase10

---
+++ script.js
@@ -0,0 +1,14 @@
+let name = "Emilia";
+let surname = "Roca";
+console.log(surname + " " + name);
+
+let numberone =120;
+let numbertwo =20;
+let resultsuma = numberone + numbertwo;
+let resultresta = numberone - numbertwo;
+let resultmulti = numberone * numbertwo;
+let resultdivision = numberone / numbertwo;
+console.log ("Suma:", resultsuma);
+console.log ("Resta:", resultresta);
+console.log ("Multiplicación:", resultmulti);
+console.log ("División:", resultdivision);
